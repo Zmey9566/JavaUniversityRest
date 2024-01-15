cc09147d35bc1d4cfd0aa8bdb008cd9fd9d91213
dlux <top_secret99@mail.ru>
2024-01-15 14:25:06 +0300

    add AdminController.java

--- src/main/java/com/example/javauniversityrest/controller/AdminController.java
+++ src/main/java/com/example/javauniversityrest/controller/AdminController.java
@@ -1,12 +1,52 @@
 package com.example.javauniversityrest.controller;
 
+import com.example.javauniversityrest.dto.read.AdminReadDto;
+import com.example.javauniversityrest.dto.save.AdminSaveDto;
+import com.example.javauniversityrest.model.Admin;
+import com.example.javauniversityrest.service.AdminServiceImpl;
+import com.example.javauniversityrest.util.MapperUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/forAdmin")
 public class AdminController {
 
-    private final Admi
+    private final AdminServiceImpl adminService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final MapperUtils<Admin, AdminReadDto, AdminSaveDto> mapperUtils;
 
+    @Autowired
+    public AdminController(AdminServiceImpl adminService, BCryptPasswordEncoder bCryptPasswordEncoder,
+                           MapperUtils<Admin, AdminReadDto, AdminSaveDto> mapperUtils) {
+        this.adminService = adminService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.mapperUtils = mapperUtils;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AdminReadDto>> showAdmins() {
+        List<AdminReadDto> admins = new ArrayList<>();
+        for (AdminReadDto admin : adminService.getAllByModel(AdminReadDto.class)) {
+            admins.add(admin);
+        }
+        return new ResponseEntity<>(admins, HttpStatus.OK);
+    }
+
+    @GetMapping("/admins/{id}")
+    public ResponseEntity<Optional<AdminReadDto>> showAdmin(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(adminService.findById(id, AdminReadDto.class), HttpStatus.OK);
+    }
+
+    
 }
