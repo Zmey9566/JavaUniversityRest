5ce851f9d59cc93f10d0798e9d04c38390b95c5e
dlux <top_secret99@mail.ru>
2024-01-19 13:50:24 +0300

    1. Override metods for Admin, Mentor, Student Services. 2. Finish tests for base metods. 3. Finish AdminController.

---
+++ src/main/java/com/example/javauniversityrest/dto/read/UserReadDto.java
@@ -0,0 +1,22 @@
+package com.example.javauniversityrest.dto.read;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@RequiredArgsConstructor
+public class UserReadDto {
+
+    private Long id;
+    private String email;
+    private RoleReadDto role;
+    private String password;
+
+    public UserReadDto(String email, RoleReadDto role, String password) {
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+}
